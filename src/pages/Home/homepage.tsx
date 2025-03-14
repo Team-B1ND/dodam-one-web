a6f2d6672cdb152a4e8f0f9b22c535f044563a20
ftery0 <lerb5253@dgsw.hs.kr>
2025-03-14 10:08:33 +0900

    feat :: home Sortable

--- src/pages/Home/homepage.tsx
+++ src/pages/Home/homepage.tsx
@@ -1,70 +1,58 @@
-import React, { useState } from "react";
-import { DndContext, DragEndEvent, closestCorners, UniqueIdentifier } from "@dnd-kit/core";
-import { arrayMove, SortableContext, useSortable } from "@dnd-kit/sortable";
+import React from "react";
+import { DndContext, closestCorners, UniqueIdentifier } from "@dnd-kit/core";
+import {  SortableContext, useSortable } from "@dnd-kit/sortable";
 import * as S from "./style";
-import Banner from "src/components/Home/Banner";
-import Schedule from "src/components/Home/Schedule";
-import TodayWakeupSong from "src/components/Home/TodayWakeupSong";
-import Meal from "src/components/Home/Meal";
-import Apply from "src/components/Home/Apply";
-import MyInfo from "src/components/Home/Myinfo";
-import { useBannerWidth } from "src/utils/Screen/useScreenWidth";
+import Banner from "components/Home/Banner";
+import Schedule from "components/Home/Schedule";
+import TodayWakeupSong from "components/Home/TodayWakeupSong";
+import Meal from "components/Home/Meal";
+import Apply from "components/Home/Apply";
+import MyInfo from "components/Home/Myinfo";
+import { useBannerWidth } from "utils/Screen/useScreenWidth";
+import { useHome } from "hooks/Home/useHome";
 
-const componentsMap: Record<string, JSX.Element> = {
-  schedule: <Schedule />,
-  todayWakeupSong: <TodayWakeupSong />,
-  meal: <Meal />,
-  apply: <Apply />,
+
+const componentsMap: Record<number, JSX.Element> = {
+  1: <Schedule />,
+  2: <TodayWakeupSong />,
+  3: <Meal />,
+  4: <Apply />,
 };
 
 const HomePage = () => {
-  const [items, setItems] = useState<string[]>(["schedule", "todayWakeupSong", "meal", "apply"]);
-  const { bannerBoxRef,bannerWidth } = useBannerWidth();
+  const {...home} = useHome();
 
-  const handleDragEnd = (event: DragEndEvent) => {
-    const { active, over } = event;
-    if (!over || active.id === over.id) return;
+  const { bannerBoxRef, bannerWidth } = useBannerWidth();
 
-    const oldIndex = items.indexOf(String(active.id));
-    const newIndex = items.indexOf(String(over.id));
+  
 
-    setItems(arrayMove(items, oldIndex, newIndex));
-  };
 
   return (
-    <DndContext collisionDetection={closestCorners} onDragEnd={handleDragEnd}>
+    <DndContext collisionDetection={closestCorners} onDragEnd={home.handleDragEnd}>
       <S.Main>
         <S.MainDataView>
           <S.BannerBox ref={bannerBoxRef}>
-              <Banner bannerWidth={bannerWidth} /> 
+            <Banner bannerWidth={bannerWidth} />
           </S.BannerBox>
-          <SortableContext items={items}>
-          <S.MainDataGridBox>
-            
+          <SortableContext items={home.items}>
+            <S.MainDataGridBox>
             <S.MainDataWidthBox>
-              {items
-                .filter((id) => ["schedule", "todayWakeupSong"].includes(id))
-                .map((id) => (
-                  <SortableItem key={id} id={id}>
-                    {componentsMap[id]}
-                  </SortableItem>
-                ))}
+              {home.items.slice(0, 2).map((id) => (
+                <SortableItem key={id} id={id}>
+                  {componentsMap[id]}
+                </SortableItem>
+              ))}
+            </S.MainDataWidthBox>
+            <S.MainDataWidthBox>
+              {home.items.slice(2, 4).map((id) => (
+                <SortableItem key={id} id={id}>
+                  {componentsMap[id]}
+                </SortableItem>
+              ))}
             </S.MainDataWidthBox>
 
-            
-            <S.MainDataWidthBox>
-              {items
-                .filter((id) => ["meal", "apply"].includes(id))
-                .map((id) => (
-                  <SortableItem key={id} id={id}>
-                    {componentsMap[id]}
-                  </SortableItem>
-                ))}
-            </S.MainDataWidthBox>
-          </S.MainDataGridBox>
-        </SortableContext>
-
-
+            </S.MainDataGridBox>
+          </SortableContext>
         </S.MainDataView>
         <S.SideProfile>
           <MyInfo />
@@ -79,25 +67,21 @@ interface SortableItemProps {
   children: React.ReactNode;
 }
 
-const SortableItem = ({ id, children }:SortableItemProps) => {
-    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
-  
-    return (
-      <S.DraggableBox
-        ref={setNodeRef}
-        style={{
-          transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : undefined,
-          transition,
-        }}
-      >
-        {/* 드래그 핸들 */}
-        <S.DragHandle {...attributes} {...listeners}></S.DragHandle>
-  
-        {/* 컨텐츠 */}
-        <div data-dnd-kit-no-drag>{children}</div>
-      </S.DraggableBox>
-    );
-  };
-  
+const SortableItem = ({ id, children }: SortableItemProps) => {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
+
+  return (
+    <S.DraggableBox
+      ref={setNodeRef}
+      style={{
+        transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : undefined,
+        transition,
+      }}
+    >
+      <S.DragHandle {...attributes} {...listeners}></S.DragHandle>
+      {children}
+    </S.DraggableBox>
+  );
+};
 
 export default HomePage;
