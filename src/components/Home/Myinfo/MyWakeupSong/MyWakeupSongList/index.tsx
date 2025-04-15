80d6ff812e47e9aab480565b8e9aa823f744ccb3
dlskl <skyandme77@gmail.com>
2025-04-15 11:34:40 +0900

    style :: fix style song #27

--- src/components/Home/Myinfo/MyWakeupSong/MyWakeupSongList/index.tsx
+++ src/components/Home/Myinfo/MyWakeupSong/MyWakeupSongList/index.tsx
@@ -1,7 +1,7 @@
 import { useGetMyWakeupSongsQuery } from "queries/WakeupSong/wakeupSong.query";
 import dataCheck from "utils/Check/dataCheck";
 import MyWakeupSongItem from "../MyWakeupSongItem";
-import { SongNullBox } from "../style";
+import { SongNullBox, WakeupSongWrap } from "../style";
 
 const MyInfoWakeupSongList = () => {
   const { data: serverMyWakeupSongData } = useGetMyWakeupSongsQuery({
@@ -17,9 +17,9 @@ const MyInfoWakeupSongList = () => {
         <SongNullBox>
           <span>기상송 신청내역이 없습니다.</span>
           <span>신청하시면 생활이 윤택해질 거에요!</span>
-          </SongNullBox>
+        </SongNullBox>
       ) : (
-        <>
+        <WakeupSongWrap>
           {serverMyWakeupSongData?.data
             .filter((wakeupSong) => wakeupSong.status === "PENDING")
             .map((wakeupSong) => (
@@ -28,7 +28,7 @@ const MyInfoWakeupSongList = () => {
                 key={wakeupSong.id}
               />
             ))}
-        </>
+        </WakeupSongWrap>
       )}
     </>
   );
